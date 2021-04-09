aac624f27d35be405551c34d29eae5bbd65c251f
David Garbanzo <77855051+davidgq10@users.noreply.github.com>
2021-04-08 21:52:05 -0600

    Entrega final

--- src/app.py
+++ src/app.py
@@ -4,12 +4,8 @@ app = Flask(__name__)
 todos = [{ "label": "My first task", "done": False }]
 
 @app.route('/todos', methods=['GET'])
-
 def hello_world():
-
     json_text = jsonify(todos)
-
-    # y luego puedes retorarla (return) en el response body así:
     return json_text
 
 @app.route('/todos', methods=['POST'])
@@ -26,8 +22,5 @@ def delete_todo(position):
     todos.pop(position)
     return jsonify(todos)
 
-
-
-# These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
